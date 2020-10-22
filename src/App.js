c7c1665950c8226b592ca9423214387b5656fb27
Shujaat Azim <shujaat.azim@gmail.com>
2020-10-22 18:38:49 -0400

    Changed all of the tutorials instructions to modern ReactJS but it's not working. Will try to copy/paste to see if the problem is the fact that it's not a class component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,76 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { ActionCable } from 'actioncable'
+
+const App = () => {
+
+  const [messages, setMessages] = useState([])
+  const [cable, setCable] = useState
+
+  useEffect(() => {
+    createCable()
+    fetchMessages()
+    createSubscription()
+  }, [])
 
 
-function App() {
+  const createCable = () => {
+    setCable(ActionCable.createConsumer('ws://localhost:3000/cable'))
+  }
+
+  const fetchMessages = () => {
+    fetch('http://localhost:3000/messages')
+      .then(res => res.json())
+      .then(messages => setMessages({ messages: messages }))
+  }
+
+  const createSubscription = () => {
+    cable.subscriptions.create(
+      { channel: 'MessagesChannel' },
+      { received: message => handleReceivedMessage(message) }
+    )
+  }
+
+  const mapMessages = () => {
+    return messages.map((message, i) => 
+      <li key={i}>{message.content}</li>)
+  }
+
+  const handleReceivedMessage = message => {
+    setMessages({ messages: [...messages, message] })
+  }
+
+  const handleMessageSubmit = e => {
+    e.preventDefault();
+    const messageObj = {
+      message: {
+        content: e.target.message.value
+      }
+    }
+    const fetchObj = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(messageObj)
+    }
+    fetch('http://localhost:3000/messages', fetchObj)
+    e.target.reset()
+  }
+
   return (
     <div>
+      <ActionCable 
+          channel={{ channel: 'MessagesChannel' }}
+          onReceived={() => handleReceivedMessage()}
+        />
+        <h2>Messages</h2>
+
+        <ul>{() => mapMessages()}</ul>
+
+        <form onSubmit={() => handleMessageSubmit()}>
+          <input name='message' type='text' />
+          <input type='submit' value='Send message' />
+        </form>
     </div>
   )
 }
